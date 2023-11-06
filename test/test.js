cf8114612538d2f7fb51aa93db5e81f9cb8380a8
Tornike Machitidze <Tornike_Machitidze@epam.com>
2023-11-07 03:26:04 +0400

    fix: delete jest

--- test/test.js
+++ test/test.js
@@ -1,6 +1,3 @@
 const Offloader = require('../lib/index');
 
-it('We can check if offloader insatnce contains a method offload', () => {
-  const offloader = new Offloader();
-  expect(typeof offloader.offload).toBe('function');
-});
+const offloader = new Offloader();
